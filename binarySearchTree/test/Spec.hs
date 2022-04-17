169bf01aadbda50b4bd9112b034abdfbe07d6f7a
Luzia Kündig <luzia.kuendig@ost.ch>
2022-04-17 18:06:47 +0200

    added 2 tests

--- binarySearchTree/test/Spec.hs
+++ binarySearchTree/test/Spec.hs
@@ -1,2 +1,15 @@
-Main :: IO ()
-Main = putStrLn "Test suite not yet implemented"
+import Test.Hspec
+import qualified BinarySearchTree
+    as Set (T, empty, insert, fromList, toList, member)
+
+main :: IO ()
+main = hspec spec
+    
+spec :: Spec
+spec = do
+    describe "BinarySearchTree Implementation" $ do
+        it "show (Set.fromList [1]) should equal \"Node Leaf 1 Leaf\"" $
+            show (Set.fromList [1]) `shouldBe` "Node Leaf 1 Leaf"
+        it "show (Set.insert 1 Set.empty) should equal \"Node Leaf 1 Leaf\"" $
+            show (Set.insert 1 Set.empty) `shouldBe` "Node Leaf 1 Leaf"
+        -- it 
